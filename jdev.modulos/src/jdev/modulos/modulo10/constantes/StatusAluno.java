b5e100be0317e83b6bf2b8f05425ebdcba6fd1db
andre_abreu <andre_abreu@04B-IN001324.agropalma.net>
2022-11-25 13:41:20 -0300

    Unificando módulo 10

---
+++ jdev.modulos/src/jdev/modulos/modulo10/constantes/StatusAluno.java
@@ -0,0 +1,8 @@
+package jdev.modulos.modulo10.constantes;
+
+//Classe e constantes estáticas
+public class StatusAluno {
+	public static String APROVADO = "Aprovado";
+	public static String RECUPERACAO = "Recuperacao";
+	public static String REPROVADO = "Reprovado";
+}
