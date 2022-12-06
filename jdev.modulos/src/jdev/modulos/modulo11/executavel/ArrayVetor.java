ca97fdcd09695be8a532d1addfa414a614349dec
aandd <aandd@ABREU_DELL>
2022-12-06 20:51:44 -0300

    13.2 - Criando nosso primeiro Array

---
+++ jdev.modulos/src/jdev/modulos/modulo11/executavel/ArrayVetor.java
@@ -0,0 +1,25 @@
+package jdev.modulos.modulo11.executavel;
+
+public class ArrayVetor {
+
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * Array pode ser de todos os tipos de dados e objetos também
+		 * 
+		 * Array sempre deve ter a quantidade de posições definidas
+		 */
+		
+		double[] notas = new double[4];
+		
+		/*Atribuir valor nas posições do array*/
+		notas[0] = 9.8;
+		notas[1] = 7.8;
+		notas[2] = 3.8;
+		notas[3] = 6.8;
+		
+		System.out.println(notas);
+		
+	}
+}
