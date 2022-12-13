8bd53fd7300433f524c196f8d2c379bba19d2b5e
andre_abreu <andre_abreu@04B-IN001324.agropalma.net>
2022-12-13 12:41:19 -0300

    13.3 - Recuperando valores no Array

--- jdev.modulos/src/jdev/modulos/modulo11/executavel/ArrayVetor.java
+++ jdev.modulos/src/jdev/modulos/modulo11/executavel/ArrayVetor.java
@@ -3,23 +3,24 @@ package jdev.modulos.modulo11.executavel;
 public class ArrayVetor {
 
 	public static void main(String[] args) {
-		
-		
+
 		/*
 		 * Array pode ser de todos os tipos de dados e objetos também
 		 * 
 		 * Array sempre deve ter a quantidade de posições definidas
 		 */
-		
-		double[] notas = new double[4];
-		
-		/*Atribuir valor nas posições do array*/
+
+		double[] notas = new double[5];
+
+		/* Atribuir valor nas posições do array */
 		notas[0] = 9.8;
 		notas[1] = 7.8;
 		notas[2] = 3.8;
 		notas[3] = 6.8;
-		
-		System.out.println(notas);
-		
+
+		for (int posit = 0; posit < 5; posit++) {
+			System.out.println("Nota " + (posit + 1) + " = " + notas[posit]);
+		}
+
 	}
 }
