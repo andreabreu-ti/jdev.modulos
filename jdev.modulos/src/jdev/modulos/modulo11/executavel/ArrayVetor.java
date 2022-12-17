459275f4386b7fc018b7f63470dc3df83bc6d6ee
aandd <aandd@ABREU_DELL>
2022-12-17 17:18:52 -0300

    Adicionando o array de notas dentro da disciplina

--- jdev.modulos/src/jdev/modulos/modulo11/executavel/ArrayVetor.java
+++ jdev.modulos/src/jdev/modulos/modulo11/executavel/ArrayVetor.java
@@ -1,38 +1,29 @@
 package jdev.modulos.modulo11.executavel;
 
-import javax.swing.JOptionPane;
+import jdev.modulos.modulo11.classes.Aluno;
+import jdev.modulos.modulo11.classes.Disciplina;
 
 public class ArrayVetor {
 
 	public static void main(String[] args) {
 
-		/*
-		 * Array pode ser de todos os tipos de dados e objetos também
-		 * 
-		 * Array sempre deve ter a quantidade de posições definidas
-		 */
+		double[] notas = { 8.8, 9.7, 7.6, 6.8 };
+		double[] notasLogica = { 8.3, 9.2, 7.1, 6.7 };
 
-		int[] inteiros = new int[10];
-		float[] floats = new float[10];
+		Aluno aluno = new Aluno();
+		aluno.setNome("André Abreu");
+		aluno.setNomeEscola("JDEV Treinamento");
+
+		Disciplina disciplina = new Disciplina();
+		disciplina.setDisciplina("Curso de Java");
+		disciplina.setNota(notas);
 		
-		String[] valores = { "andre", "90", "Curso de Java", "andre@gmail.com" };
+		aluno.getDisciplinas().add(disciplina);
+		
+		Disciplina disciplina2 = new Disciplina();
+		disciplina2.setDisciplina("Lógica de Programação");
+		disciplina2.setNota(notasLogica);
 
-		for (int pos = 0; pos < valores.length; pos++) {
-			System.out.println("Valor na posição " + pos + " é igual = " + valores[pos]);
-		}
-
-//		String posicoes = JOptionPane.showInputDialog("Quantas posições o array deve ter?");
-//
-//		double[] notas = new double[Integer.parseInt(posicoes)];
-//
-//		for (int pos = 0; pos < notas.length; pos++) {
-//			String valor = JOptionPane.showInputDialog("Qual o valor da posição " + pos + "?");
-//			notas[pos] = Double.valueOf(valor);
-//		}
-//
-//		for (int pos = 0; pos < notas.length; pos++) {
-//			System.out.println("Nota " + (pos + 1) + " = " + notas[pos]);
-//		}
-
+		aluno.getDisciplinas().add(disciplina2);
 	}
 }
