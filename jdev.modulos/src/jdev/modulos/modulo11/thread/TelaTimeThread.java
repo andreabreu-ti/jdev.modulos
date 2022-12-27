bead12e15d3ace3dca07024facb115e96cc53891
aandd <aandd@ABREU_DELL>
2022-12-27 20:20:23 -0300

    14.9-Controlando a Thead 1 com Start e Stop

--- jdev.modulos/src/jdev/modulos/modulo11/thread/TelaTimeThread.java
+++ jdev.modulos/src/jdev/modulos/modulo11/thread/TelaTimeThread.java
@@ -5,6 +5,10 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -20,9 +24,29 @@ public class TelaTimeThread extends JDialog {
 
 	private JLabel descricaoHora2 = new JLabel("Time Thread 2");
 	private JTextField mostraTempo2 = new JTextField();
-	
+
 	private JButton jButton = new JButton("Start");
 	private JButton jButton2 = new JButton("Stop");
+
+	private Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+			while (true) {
+				mostraTempo
+						.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		}
+	};
+
+	private Thread thread1Time;
 
 	public TelaTimeThread() { // Constructor executa o que tiver dentro no momento da abertura ou execução
 
@@ -49,26 +73,42 @@ public class TelaTimeThread extends JDialog {
 		descricaoHora2.setPreferredSize(new Dimension(200, 25));
 		gridBagConstraints.gridy++;
 		jPanel.add(descricaoHora2, gridBagConstraints);
-		
-		mostraTempo2.setPreferredSize(new Dimension(200,25));
+
+		mostraTempo2.setPreferredSize(new Dimension(200, 25));
 		gridBagConstraints.gridy++;
 		mostraTempo2.setEditable(false);
 		jPanel.add(mostraTempo2, gridBagConstraints);
 
-		
-		
 		gridBagConstraints.gridwidth = 1;
-		
+
 		jButton.setPreferredSize(new Dimension(92, 25));
-		gridBagConstraints.gridy ++;
+		gridBagConstraints.gridy++;
 		jPanel.add(jButton, gridBagConstraints);
-		
-		jButton2.setPreferredSize(new Dimension(92,25));
-		gridBagConstraints.gridx ++;
+
+		jButton2.setPreferredSize(new Dimension(92, 25));
+		gridBagConstraints.gridx++;
 		jPanel.add(jButton2, gridBagConstraints);
+
+		jButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				thread1Time = new Thread(thread1);
+				thread1Time.start();
+
+			}
+		});
 		
-		
-		
+		jButton2.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				thread1Time.stop();
+				
+			}
+		});
+
 		add(jPanel, BorderLayout.WEST);
 		// Sempre será o último comando
 		setVisible(true);// Torna a tela visivel para o usuário
