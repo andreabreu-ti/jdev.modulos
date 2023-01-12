5eaec2f4555096a9098ebf73d8a6432de2c224fb
aandd <aandd@ABREU_DELL>
2023-01-12 20:00:43 -0300

    16.03-Lendo arquivo TXT e criando lista de objetos

--- jdev.modulos/src/jdev/modulos/modulo16/LerArquivoTxt.java
+++ jdev.modulos/src/jdev/modulos/modulo16/LerArquivoTxt.java
@@ -3,23 +3,40 @@ package jdev.modulos.modulo16;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LerArquivoTxt {
 
 	public static void main(String[] args) throws FileNotFoundException {
 
-		FileInputStream entradaArquivo = new FileInputStream(new File(
-				"C:\\Users\\andre_abreu\\git\\jdev.modulos\\jdev.modulos\\src\\jdev\\modulos\\modulo16\\arquivowork.txt"));
+//		FileInputStream entradaArquivo = new FileInputStream(new File(
+//				"C:\\Users\\andre_abreu\\git\\jdev.modulos\\jdev.modulos\\src\\jdev\\modulos\\modulo16\\arquivowork.txt"));
 
+		FileInputStream entradaArquivo = new FileInputStream(new File(
+				"C:\\Users\\aandd\\git\\jdev.modulos\\jdev.modulos\\src\\jdev\\modulos\\modulo16\\arquivohome.txt"));
+		
 		Scanner lerArquivo = new Scanner(entradaArquivo, "UTF-8");
 
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
 		while (lerArquivo.hasNext()) {
 			String linha = lerArquivo.nextLine();
 			if (linha != null && !linha.isEmpty()) {
-				System.out.println(linha);
+
+				String[] dados = linha.split("\\|");
+				Pessoa pessoa = new Pessoa();
+				pessoa.setNome(dados[0]);
+				pessoa.setEmail(dados[1]);
+				pessoa.setIdade(Integer.parseInt(dados[2]));
+				
+				pessoas.add(pessoa);
 			}
 
+		}
+		
+		for(Pessoa pessoa : pessoas) {
+			System.out.println(pessoa);
 		}
 	}
 }
